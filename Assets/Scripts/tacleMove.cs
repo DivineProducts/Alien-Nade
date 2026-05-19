cc809f9142b12a62fbbce35e684d9058d5b317cd
DivineProducts <thedivineproductsofficial@gmail.com>
2026-05-19 18:22:09 +0300

    handGrab
    added the lefthand grabbing "Grabbable" tagged objects

--- Assets/Scripts/tacleMove.cs
+++ Assets/Scripts/tacleMove.cs
@@ -9,10 +9,25 @@ public class tacleMove : MonoBehaviour
     private bool urmareste = false;
     private bool stop = false;
 
+
+    //grabbing stuff
+    private GameObject grabbedOb;
+    private bool tine = false;
     void Update()
     {   // doar il face sa devina inapoi dynamic ;  restul se reactiveaza de la sine
         if (Input.GetKeyUp(KeyCode.Z))
         {
+            if (grabbedOb != null)
+            {
+                grabbedOb.GetComponent<BoxCollider2D>().isTrigger = false;
+                grabbedOb = null;
+            }
+            if (tine)
+            {
+                // Release the object
+                /* grabbedOb.transform.parent = null;*/
+                tine = false;
+            }
             this.GetComponent<SpriteRenderer>().sprite = straight;
             GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
         }
@@ -20,10 +35,17 @@ public class tacleMove : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Z))
         {
             this.GetComponent<SpriteRenderer>().sprite = curved;
-            urmareste = false;
+            /*urmareste = false;
             GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
             GetComponent<Rigidbody2D>().angularVelocity = 0f;
-            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;*/
+            if (grabbedOb != null)
+            {
+                // Grab the object
+               /* grabbedOb.transform.parent = transform;*/
+                tine = true;
+            }
+
         }
 
         // uff, 0=LeftMouse; 1=RightMouse.... bagamias piciarele
@@ -49,5 +71,37 @@ public class tacleMove : MonoBehaviour
             Vector2 targetPos = mouseReal2 + distObMous;
             GetComponent<Rigidbody2D>().MovePosition(Vector2.Lerp(GetComponent<Rigidbody2D>().position, targetPos, followSpeed * Time.fixedDeltaTime));
         }
+        if (tine == true)
+        {
+            if (grabbedOb != null)
+                if (grabbedOb.GetComponent<BoxCollider2D>().isTrigger == false)
+                    grabbedOb.GetComponent<BoxCollider2D>().isTrigger = true;
+            
+            Vector3 mouseReal2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 targetPos = mouseReal2 + distObMous;
+            grabbedOb.GetComponent<Rigidbody2D>().MovePosition(Vector2.Lerp(GetComponent<Rigidbody2D>().position, targetPos, followSpeed * Time.fixedDeltaTime));
+        }
+
     }
+
+    //Grabbing stuff
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Grabbable"))
+        {
+            grabbedOb = other.gameObject;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == grabbedOb)
+        {
+            if (!tine)
+            {
+                grabbedOb.GetComponent<BoxCollider2D>().isTrigger = false;
+                grabbedOb = null;
+            }
+        }
+    }
+
 }
