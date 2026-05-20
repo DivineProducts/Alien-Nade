72a6c8ac6a8e43a8c3acf6824dd01d379706aefa
Păduraru David <padurarudavid.dp@gmail.com>
2026-05-21 02:07:27 +0300

    Telefon(Aproape complet)

--- Assets/Scripts/pahareControl.cs
+++ Assets/Scripts/pahareControl.cs
@@ -5,7 +5,7 @@ public class pahareControl : MonoBehaviour
     public int pahareRamase;
     public Sprite jumate;
     public Sprite treime;
-    
+    public Sprite plin;
     public void scade()
     {
         pahareRamase--;
@@ -13,6 +13,8 @@ public class pahareControl : MonoBehaviour
     public void schimbaSprite()
     {
         pahareRamase--;
+        if (pahareRamase == 6)
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = plin;
         if (pahareRamase == 4)
             this.gameObject.GetComponent<SpriteRenderer>().sprite = jumate;
         if (pahareRamase == 2)
