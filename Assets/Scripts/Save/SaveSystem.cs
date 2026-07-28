fff90e544aab33a1a2678a9be9437dbbb3c75048
DivineProducts <thedivineproductsofficial@gmail.com>
2026-07-28 21:46:40 +0300

    micbinar

--- Assets/Scripts/Save/SaveSystem.cs
+++ Assets/Scripts/Save/SaveSystem.cs
@@ -1,10 +1,12 @@
 using System.IO;
 using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
 public static class SaveSystem
 {
     private static string path = Application.persistentDataPath + "/save.json";
     public static void Save(Vector3 baniscale, Vector3 banipos, int gheata, int pahar, int zahar, int lamaie, int kiwi, int portocala)
     {
+        BinaryFormatter formater = new BinaryFormatter();
         GameData data = new GameData(baniscale, banipos,gheata,pahar,zahar,lamaie,kiwi,portocala);
         string json = JsonUtility.ToJson(data, true);
         File.WriteAllText(path, json);
